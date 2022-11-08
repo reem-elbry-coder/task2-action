8dcb3b9063761c177d3e12accb476301af180d90
Your Name <you@example.com>
2022-11-08 06:39:53 -0800

    added code files

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM openjdk
+
+WORKDIR /application
+
+COPY task1.java
+
+RUN javac task1.java
+
+CMD java task1
---
+++ task1.java
@@ -0,0 +1,4 @@
+public class task1 {
+    public static void main (String []args){
+    System.out.println("task1");
+}}
